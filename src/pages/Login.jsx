9f406440b2eb2cf3f09dfc9b07bb7e33127ef135
Woollywood <115595112+Woollywood@users.noreply.github.com>
2024-08-03 17:45:58 +0300

    re: login form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,20 +1,20 @@
-import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import { AuthService } from '../services/AuthService';
 import { useAuthDispatch } from '../contexts/AuthContext';
+import { useForm } from 'react-hook-form';
 
 export function Component() {
-	const [username, setUsername] = useState('');
-	const [password, setPassword] = useState('');
 	const navigate = useNavigate();
-
 	const dispatch = useAuthDispatch();
-
-	async function handleSubmit(e) {
-		e.preventDefault();
-
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm();
+	console.log(errors);
+	async function onSubmit({ username, password }) {
 		try {
 			const data = await AuthService.login(username, password);
 			dispatch({ type: 'SET_USER', payload: data });
@@ -26,22 +26,26 @@ export function Component() {
 
 	return (
 		<div className='flex items-center justify-center h-full'>
-			<form onSubmit={handleSubmit} className='flex flex-col items-center gap-8'>
+			<form onSubmit={handleSubmit(onSubmit)} className='flex flex-col items-center gap-8'>
 				<div className='flex flex-col items-center gap-4'>
 					<TextField
 						label='Username'
 						variant='standard'
 						type='text'
-						value={username}
-						onChange={(e) => setUsername(e.target.value)}
+						{...register('username', {
+							required: true,
+						})}
 					/>
+					{errors.username && <h2 className='text-red-700 text-sm font-medium self-start'>Required*</h2>}
 					<TextField
 						label='Password'
 						variant='standard'
 						type='password'
-						value={password}
-						onChange={(e) => setPassword(e.target.value)}
+						{...register('password', {
+							required: true,
+						})}
 					/>
+					{errors.password && <h2 className='text-red-700 text-sm font-medium self-start'>Required*</h2>}
 				</div>
 
 				<Button variant='contained' type='submit'>
